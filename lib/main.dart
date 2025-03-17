533ac5d0f4d14009cb15291e236f7a81f704368c
Luca Leuenberger <Luca.Leuenberger@bossinfo.ch>
2025-03-17 15:17:31 +0100

    Implement dynamic item management in Lucky Wheel with adjustable item count and user-defined prizes

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,41 @@ class LuckyWheel extends StatefulWidget {
   _LuckyWheelState createState() => _LuckyWheelState();
 }
 
+
 class _LuckyWheelState extends State<LuckyWheel> {
-  final List<String> items = ['Prize 1', 'Prize 2', 'Prize 3', 'Prize 4', 'Prize 5'];
   final StreamController<int> _controller = StreamController<int>();
+  final List<TextEditingController> _itemControllers = [];
+  final List<FocusNode> _focusNodes = [];
+  int _numItems = 5;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeItems();
+  }
+
+  void _initializeItems() {
+    _itemControllers.clear();
+    _focusNodes.clear();
+    for (int i = 0; i < _numItems; i++) {
+      final controller = TextEditingController(text: 'Prize ${i + 1}');
+      controller.addListener(() {
+        setState(() {});
+      });
+      _itemControllers.add(controller);
+      _focusNodes.add(FocusNode());
+    }
+  }
 
   @override
   void dispose() {
     _controller.close();
+    for (var controller in _itemControllers) {
+      controller.dispose();
+    }
+    for (var focusNode in _focusNodes) {
+      focusNode.dispose();
+    }
     super.dispose();
   }
 
@@ -39,6 +67,46 @@ class _LuckyWheelState extends State<LuckyWheel> {
         title: Text('Lucky Wheel'),
         centerTitle: true,
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text('Settings'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Number of Items'),
+              subtitle: Slider(
+                value: _numItems.toDouble(),
+                min: 2,
+                max: 20,
+                divisions: 18,
+                label: _numItems.toString(),
+                onChanged: (value) {
+                  setState(() {
+                    _numItems = value.toInt();
+                    _initializeItems();
+                  });
+                },
+              ),
+            ),
+            for (int i = 0; i < _numItems; i++)
+              ListTile(
+                title: TextField(
+                  controller: _itemControllers[i],
+                  focusNode: _focusNodes[i],
+                  decoration: InputDecoration(labelText: 'Item ${i + 1}'),
+                  onTap: () {
+                    _focusNodes[i].requestFocus();
+                  },
+                ),
+              ),
+          ],
+        ),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -46,15 +114,17 @@ class _LuckyWheelState extends State<LuckyWheel> {
             height: 300,
             child: FortuneWheel(
               items: [
-                for (var item in items) FortuneItem(child: Text(item)),
+                for (var controller in _itemControllers)
+                  FortuneItem(child: Text(controller.text)),
               ],
               selected: _controller.stream,
+              animateFirst: false,
             ),
           ),
           SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
-              _controller.add(Random().nextInt(items.length));
+              _controller.add(Random().nextInt(_numItems));
             },
             child: Text('Spin'),
           ),
